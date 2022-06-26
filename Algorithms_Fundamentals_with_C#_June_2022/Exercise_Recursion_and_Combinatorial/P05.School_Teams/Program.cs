b32e33318febfd8f3ad5b4233d10b537faef9aac
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-06-26 18:12:06 +0300

    Problems 5 and 6 solutions

---
+++ Algorithms_Fundamentals_with_C#_June_2022/Exercise_Recursion_and_Combinatorial/P05.School_Teams/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.School_Teams
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var girlsNames = Console.ReadLine().Split(", ");
+            string[] girlsCombination = new string[3];
+
+            var boysNames = Console.ReadLine().Split(", ");
+            string[] boysCombination = new string[2];
+
+            var girlsCombinations = new List<string[]>();
+            var boysCombinations = new List<string[]>();
+
+
+            Combinations(0, 0, girlsNames, girlsCombination, girlsCombinations);
+            Combinations(0, 0, boysNames, boysCombination, boysCombinations);
+
+            foreach (var girlCombination in girlsCombinations)
+            {
+                foreach (var boyCombination in boysCombinations)
+                {
+                    Console.WriteLine(string.Join(", ", girlCombination) + ", " + string.Join(", ", boyCombination));
+                }
+            }
+        }
+
+        private static void Combinations(int index, int start, string[] names, string[] combination, List<string[]> combinations)
+        {
+            if(index >= combination.Length)
+            {
+                combinations.Add(combination.ToArray());
+                return;
+            }
+
+            for (int i = start; i < names.Length; i++)
+            {
+                combination[index] = names[i];
+                Combinations(index + 1, i + 1, names, combination, combinations);
+            }
+        }
+    }
+}
---
+++ Algorithms_Fundamentals_with_C#_June_2022/Exercise_Recursion_and_Combinatorial/P06.Word_Cruncher/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06.Word_Cruncher
+{
+    internal class Program
+    {
+        private static string[] words;
+        private static string targetWord;
+        private static Dictionary<int, List<string>> wordsByIndex;
+        private static Dictionary<string, int> wordsByCount;
+        private static LinkedList<string> usedWords;
+
+        static void Main()
+        {
+            words = Console.ReadLine().Split(", ");
+            targetWord = Console.ReadLine();
+            wordsByIndex = new Dictionary<int, List<string>>();
+            wordsByCount = new Dictionary<string, int>();
+            usedWords = new LinkedList<string>();
+
+            foreach (var word in words)
+            {
+                int index = targetWord.IndexOf(word);
+
+                if (index == -1)
+                    continue;
+
+                if (wordsByCount.ContainsKey(word))
+                {
+                    wordsByCount[word]++;
+                    continue;
+                }
+
+                wordsByCount[word] = 1;
+
+                while (index != -1)
+                {
+                    if (!wordsByIndex.ContainsKey(index))
+                        wordsByIndex[index] = new List<string>();
+
+                    wordsByIndex[index].Add(word);
+
+                    index = targetWord.IndexOf(word, index + 1);
+                }
+            }
+
+            GenSolutions(0);
+        }
+
+        private static void GenSolutions(int index)
+        {
+            if(index == targetWord.Length)
+            {
+                Console.WriteLine(string.Join(" ", usedWords));
+                return;
+            }
+
+            if (!wordsByIndex.ContainsKey(index))
+                return;
+
+            foreach (var word in wordsByIndex[index])
+            {
+                if (wordsByCount[word] == 0)
+                    continue;
+
+                wordsByCount[word]--;
+                usedWords.AddLast(word);
+
+                GenSolutions(index + word.Length);
+
+                wordsByCount[word]++;
+                usedWords.RemoveLast();
+            }
+        }
+    }
+}
