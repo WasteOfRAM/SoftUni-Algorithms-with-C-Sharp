6bc1db6cd424f8ab435c31b169fa1c456abfe9ce
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-06-26 12:18:23 +0300

    Setup and problem 1 solution

---
+++ Algorithms_Fundamentals_with_C#_June_2022/Exercise_Recursion_and_Combinatorial_Algorithms/P01.Reverse_Array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P01.Reverse_Array
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var arr = Console.ReadLine().Split();
+
+            ArrayReverse(0, arr);
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        private static void ArrayReverse<T>(int index, params T[] arr)
+        {
+            if(arr.Length / 2 == index)
+                return;
+
+            var temp = arr[index];
+            arr[index] = arr[arr.Length - 1 - index];
+            arr[arr.Length - 1 - index] = temp;
+
+            ArrayReverse(index + 1, arr);
+        }
+    }
+}
