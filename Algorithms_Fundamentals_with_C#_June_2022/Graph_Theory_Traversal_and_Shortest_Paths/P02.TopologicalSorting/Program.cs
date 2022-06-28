908686c08d96a75ca399ec2f38720de5df5a4a8d
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-06-29 00:38:30 +0300

    Problem 2 solution

---
+++ Algorithms_Fundamentals_with_C#_June_2022/Graph_Theory_Traversal_and_Shortest_Paths/P02.TopologicalSorting/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//Source Removal Algorithm
+
+namespace P02.Topological
+{
+    internal class Program
+    {
+        private static Dictionary<string, List<string>> graph;
+        private static Dictionary<string, int> predecessorCount;
+        private static List<string> sorted;
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            graph = ReadGraph(n);
+            predecessorCount = GetPredecessors(graph);
+
+            TopologicalSort();
+            
+            if(predecessorCount.Count > 0)
+                Console.WriteLine("Invalid topological sorting");
+            else
+                Console.WriteLine($"Topological sorting: {string.Join(", ", sorted)}");
+        }
+
+        private static void TopologicalSort()
+        {
+            sorted = new List<string>();
+
+            while (predecessorCount.Count > 0)
+            {
+                var node = predecessorCount.FirstOrDefault(n => n.Value == 0);
+
+                if (node.Key == null)
+                    break;
+
+                foreach (var child in graph[node.Key])
+                {
+                    predecessorCount[child]--;
+                }
+
+                sorted.Add(node.Key);
+                predecessorCount.Remove(node.Key);
+            }
+        }
+
+        private static Dictionary<string, int> GetPredecessors(Dictionary<string, List<string>> graph)
+        {
+            var result = new Dictionary<string, int>();
+
+            foreach (var (node, children) in graph)
+            {
+                if(!result.ContainsKey(node))
+                    result[node] = 0;
+
+                foreach (var child in children)
+                {
+                    if (!result.ContainsKey(child))
+                        result[child] = 1;
+                    else
+                        result[child]++;
+                }
+            }
+
+            return result;
+        }
+
+        private static Dictionary<string, List<string>> ReadGraph(int n)
+        {
+            var result = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var parts = Console.ReadLine().Split("->", StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToArray();
+
+                if (parts.Length == 1)
+                    result[parts[0]] = new List<string>();
+                else
+                    result[parts[0]] = parts[1].Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Algorithms_Fundamentals_with_C#_June_2022/Graph_Theory_Traversal_and_Shortest_Paths/P02a.DFS_TopologicalSorting/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//DFS Topological Sorting
+
+namespace P02.Topological
+{
+    internal class Program
+    {
+        private static Dictionary<string, List<string>> graph;
+        private static HashSet<string> visited;
+        private static HashSet<string> cycles;
+        private static Stack<string> sorted;
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            graph = ReadGraph(n);
+
+            visited = new HashSet<string>();
+            cycles = new HashSet<string>();
+            sorted = new Stack<string>();
+
+            bool isCyclic = false;
+
+            foreach (var node in graph.Keys)
+            {
+                try
+                {
+                    TopSortDFS(node);
+                }
+                catch (InvalidOperationException)
+                {
+                    isCyclic = true;
+                    break;
+                }
+            }
+
+            if (isCyclic)
+                Console.WriteLine("Invalid topological sorting");
+            else
+                Console.WriteLine($"Topological sorting: {string.Join(", ", sorted)}");
+        }
+
+        private static void TopSortDFS(string node)
+        {
+            if (cycles.Contains(node))
+                throw new InvalidOperationException("Error");
+
+            if (visited.Contains(node))
+                return;
+
+            cycles.Add(node);
+            visited.Add(node);
+
+            foreach (var child in graph[node])
+            {
+                TopSortDFS(child);
+            }
+
+            sorted.Push(node);
+            cycles.Remove(node);
+        }
+
+        private static Dictionary<string, List<string>> ReadGraph(int n)
+        {
+            var result = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var parts = Console.ReadLine().Split("->", StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToArray();
+                string key = parts[0];
+
+                if (parts.Length == 1)
+                {
+                    result[key] = new List<string>();
+                }
+                else
+                {
+                    var children = parts[1].Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+                    result[key] = children;
+                }
+            }
+
+            return result;
+        }
+    }
+}
