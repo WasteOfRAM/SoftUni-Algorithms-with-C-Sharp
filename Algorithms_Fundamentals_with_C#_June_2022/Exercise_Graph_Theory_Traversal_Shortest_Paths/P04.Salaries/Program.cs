6f0c34bdd9472bd297a56cb8d2a1b25b6b237afb
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-07-02 23:43:10 +0300

    Problems 4, 5 and 6 solutions

---
+++ Algorithms_Fundamentals_with_C#_June_2022/Exercise_Graph_Theory_Traversal_Shortest_Paths/P04.Salaries/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P04.Salaries
+{
+    internal class Program
+    {
+        private static List<int>[] graph;
+        private static Dictionary<int, int> salarySheet;
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            graph = new List<int>[n];
+            salarySheet = new Dictionary<int, int>();
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                graph[i] = new List<int>();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine();
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (line[j] == 'Y')
+                        graph[i].Add(j);
+                }
+            }
+
+            int totalSalary = 0;
+
+            for (int index = 0; index < graph.Length; index++)
+            {
+                totalSalary += DFS(index);
+            }
+
+            Console.WriteLine(totalSalary);
+        }
+
+        private static int DFS(int node)
+        {
+            if (salarySheet.ContainsKey(node))
+                return salarySheet[node];
+
+            if (graph[node].Count == 0)
+            {
+                salarySheet[node] = 1;
+                return salarySheet[node];
+            }
+
+            salarySheet[node] = 0;
+            foreach (var child in graph[node])
+            {
+                salarySheet[node] += DFS(child);
+            }
+
+            return salarySheet[node];
+        }
+    }
+}
---
+++ Algorithms_Fundamentals_with_C#_June_2022/Exercise_Graph_Theory_Traversal_Shortest_Paths/P06.Road_Reconstruction/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P06.Road_Reconstruction
+{
+    internal class Program
+    {
+        private static List<int>[] graph;
+        private static List<Edge> edges;
+        private static bool[] visited;
+
+        static void Main()
+        {
+            int buildings = int.Parse(Console.ReadLine());
+            int streets = int.Parse(Console.ReadLine());
+
+            graph = new List<int>[buildings];
+            edges = new List<Edge>();
+            
+
+            for (int building = 0; building < buildings; building++)
+            {
+                graph[building] = new List<int>();
+            }
+
+            for (int i = 0; i < streets; i++)
+            {
+                var street = Console.ReadLine().Split(" - ").Select(int.Parse).ToArray();
+
+                int first = street[0];
+                int second = street[1];
+
+                graph[first].Add(second);
+                graph[second].Add(first);
+
+                edges.Add(new Edge { First = first, Second = second });
+            }
+
+            Console.WriteLine("Important streets:");
+
+            foreach (var edge in edges)
+            {
+                graph[edge.First].Remove(edge.Second);
+                graph[edge.Second].Remove(edge.First);
+
+                visited = new bool[buildings];
+
+                DFS(0);
+
+                if(visited.Contains(false))
+                    Console.WriteLine(edge);
+
+                graph[edge.First].Add(edge.Second);
+                graph[edge.Second].Add(edge.First);
+            }
+        }
+
+        private static void DFS(int node)
+        {
+            if (visited[node])
+                return;
+
+            visited[node] = true;
+
+            foreach (var child in graph[node])
+            {
+                DFS(child);
+            }
+        }
+    }
+
+    class Edge
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+
+        public override string ToString()
+        {
+            if (First < Second)
+                return $"{First} {Second}";
+
+            return $"{Second} {First}";
+        }
+    }
+}
