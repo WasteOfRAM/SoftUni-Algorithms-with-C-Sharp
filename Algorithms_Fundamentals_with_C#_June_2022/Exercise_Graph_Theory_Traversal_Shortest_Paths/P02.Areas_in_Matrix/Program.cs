697457a44a116e7987ec8519b02eb7f4d1f0210f
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-07-02 21:16:28 +0300

    Problems 2, 3 solutions

---
+++ Algorithms_Fundamentals_with_C#_June_2022/Exercise_Graph_Theory_Traversal_Shortest_Paths/P02.Areas_in_Matrix/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02.Areas_in_Matrix
+{
+    internal class Program
+    {
+        private static char[,] graph;
+        private static SortedDictionary<char, int> areas;
+        private static bool[,] visited;
+
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+
+            int totalAreas = 0;
+
+            graph = new char[rows, cols];
+            areas = new SortedDictionary<char, int>();
+            visited = new bool[rows, cols];
+
+            for (int row = 0; row < graph.GetLength(0); row++)
+            {
+                var line = Console.ReadLine();
+
+                for (int col = 0; col < graph.GetLength(1); col++)
+                {
+                    graph[row, col] = line[col];
+                }
+            }
+
+            for (int startRow = 0; startRow < graph.GetLength(0); startRow++)
+            {
+                for (int startCol = 0; startCol < graph.GetLength(1); startCol++)
+                {
+                    char areaType = graph[startRow, startCol];
+
+                    if (!visited[startRow, startCol])
+                    {
+                        DFS(startRow, startCol, areaType);
+
+                        if (!areas.ContainsKey(areaType))
+                            areas[areaType] = 0;
+
+                        areas[areaType]++;
+
+                        totalAreas++;
+                    }
+
+                }
+            }
+
+            Console.WriteLine($"Areas: {totalAreas}");
+            foreach (var (k, v) in areas)
+            {
+                Console.WriteLine($"Letter '{k}' -> {v}");
+            }
+        }
+
+        private static void DFS(int row, int col, char areaType)
+        {
+            if (row < 0 || row >= graph.GetLength(0) || col < 0 || col >= graph.GetLength(1))
+                return;
+
+            if (graph[row, col] != areaType)
+                return;
+
+            if (visited[row, col])
+                return;
+
+            visited[row, col] = true;
+
+            DFS(row, col - 1, areaType);
+            DFS(row, col + 1, areaType);
+            DFS(row - 1, col, areaType);
+            DFS(row + 1, col, areaType);
+        }
+    }
+}
---
+++ Algorithms_Fundamentals_with_C#_June_2022/Exercise_Graph_Theory_Traversal_Shortest_Paths/P03.Cycles_in_Graph/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.Cycles_in_Graph
+{
+    internal class Program
+    {
+        private static Dictionary<string, List<string>> graph;
+        private static HashSet<string> visited;
+
+        static void Main()
+        {
+            graph = new Dictionary<string, List<string>>();
+
+            string edge;
+            while ((edge = Console.ReadLine()) != "End")
+            {
+                var edgeSplit = edge.Split("-").ToArray();
+                var parent = edgeSplit[0];
+                var child = edgeSplit[1];
+
+                if(!graph.ContainsKey(parent))
+                    graph[parent] = new List<string>();
+
+                if (!graph.ContainsKey(child))
+                    graph[child] = new List<string>();
+
+                graph[parent].Add(child);
+            }
+
+            bool isCyclic = false;
+
+            foreach (var vert in graph)
+            {
+                if ((isCyclic = BFS(vert.Key)) == true)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Acyclic: {(isCyclic ? "No" : "Yes")}");
+        }
+
+        private static bool BFS(string start)
+        {
+            var queue = new Queue<string>();
+            queue.Enqueue(start);
+
+            visited = new HashSet<string> { start };
+            var cyclic = new HashSet<string> { start };
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                foreach (var child in graph[node])
+                {
+                    if (cyclic.Contains(child))
+                        return true;
+
+                    if (visited.Contains(child))
+                        continue;
+
+                    queue.Enqueue(child);
+                    visited.Add(child);
+                    cyclic.Add(child);
+                }
+            }
+
+            return false;
+        }
+    }
+}
