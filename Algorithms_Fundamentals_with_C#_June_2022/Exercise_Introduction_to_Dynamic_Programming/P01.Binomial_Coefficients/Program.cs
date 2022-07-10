3ee2cde7a0388d1ee6aa3855c59ece3f86ec6d3e
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-07-10 17:26:18 +0300

    Setup and problems 1, 2 solutions.

---
+++ Algorithms_Fundamentals_with_C#_June_2022/Exercise_Introduction_to_Dynamic_Programming/P01.Binomial_Coefficients/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P01.Binomial_Coefficients
+{
+    internal class Program
+    {
+        private static long[,] mem; 
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            mem = new long[n + 1, k + 1];
+            mem[0, 0] = 1;
+
+            long binomialCoefficient = BinomialCoefficient(n, k);
+
+            Console.WriteLine(binomialCoefficient);
+        }
+
+        private static long BinomialCoefficient(int n, int k)
+        {
+            if (mem[n, k] != 0)
+                return mem[n, k];
+
+            if (k == 0 || k == n)
+            {
+                mem[n, k] = 1;
+                return mem[n, k];
+            }
+
+            mem[n, k] = BinomialCoefficient(n - 1, k - 1) + BinomialCoefficient(n - 1, k);
+
+            return mem[n, k];
+        }
+    }
+}
---
+++ Algorithms_Fundamentals_with_C#_June_2022/Exercise_Introduction_to_Dynamic_Programming/P02.Dividing_Presents/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.Dividing_Presents
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] presents = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int presentsTotalValue = presents.Sum();
+
+            var posibleSums = GetPosibleSums(presents);
+
+            var alanSum = presentsTotalValue / 2;
+
+            while (true)
+            {
+                if (posibleSums.ContainsKey(alanSum))
+                {
+                    var alanTakes = Subset(posibleSums, alanSum);
+
+                    var bobSum = presentsTotalValue - alanSum;
+                    Console.WriteLine($"Difference: {bobSum - alanSum}");
+                    Console.WriteLine($"Alan:{alanSum} Bob:{bobSum}");
+                    Console.WriteLine($"Alan takes: {string.Join(" ", alanTakes)}");
+                    Console.WriteLine($"Bob takes the rest.");
+                    break;
+                }
+
+                alanSum -= 1;
+            }
+        }
+
+        private static List<int> Subset(IDictionary<int, int> posibleSums, int target)
+        {
+            var subset = new List<int>();
+
+            while (target != 0)
+            {
+                var sum = posibleSums[target];
+                subset.Add(sum);
+                target -= sum;
+            }
+
+            return subset;
+        }
+
+        private static IDictionary<int, int> GetPosibleSums(int[] nums)
+        {
+            var posibleSums = new Dictionary<int, int> { { 0, 0 } };
+
+            foreach (var num in nums)
+            {
+                var newSums = new Dictionary<int, int>();
+
+                foreach (var sum in posibleSums.Keys)
+                {
+                    var newSum = num + sum;
+                    if (!posibleSums.ContainsKey(newSum))
+                        newSums[newSum] = num;
+                }
+
+                foreach (var sum in newSums)
+                {
+                    posibleSums.Add(sum.Key, sum.Value);
+                }
+            }
+
+            return posibleSums;
+        }
+    }
+}
