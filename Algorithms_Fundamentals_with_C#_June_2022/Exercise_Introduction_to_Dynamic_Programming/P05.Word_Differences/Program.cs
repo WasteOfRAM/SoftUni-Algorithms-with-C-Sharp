406fbf6372e1656d240957fa9b48a9467efcf3e5
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-07-10 22:19:22 +0300

    Problems 5, 6 and 7 solutions

---
+++ Algorithms_Fundamentals_with_C#_June_2022/Exercise_Introduction_to_Dynamic_Programming/P05.Word_Differences/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P05.Word
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+
+            var dp = new int[str1.Length + 1, str2.Length + 1];
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                dp[row, 0] = row;
+            }
+
+            for (int col = 1; col < dp.GetLength(1); col++)
+            {
+                dp[0, col] = col;
+            }
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                for (int col = 1; col < dp.GetLength(1); col++)
+                {
+                    if (str1[row -1] == str2[col -1])
+                        dp[row, col] = dp[row - 1, col - 1];
+                    else
+                        dp[row, col] = Math.Min(dp[row - 1, col], dp[row, col - 1]) + 1;
+                }
+            }
+
+            Console.WriteLine($"Deletions and Insertions: {dp[str1.Length, str2.Length]}");
+        }
+    }
+}
---
+++ Algorithms_Fundamentals_with_C#_June_2022/Exercise_Introduction_to_Dynamic_Programming/P07.Minimum_Edit_Distance/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace P07.Minimum_Edit_Distance
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int replaceCost = int.Parse(Console.ReadLine());
+            int incertCost = int.Parse(Console.ReadLine());
+            int deleteCost = int.Parse(Console.ReadLine());
+
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+
+            var dp = new int[str1.Length + 1, str2.Length + 1];
+
+            for (int col = 1; col < dp.GetLength(1); col++)
+            {
+                dp[0, col] = dp[0, col - 1] + incertCost;
+            }
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                dp[row, 0] = dp[row - 1, 0] + deleteCost;
+            }
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                for (int col = 1; col < dp.GetLength(1); col++)
+                {
+                    if (str1[row - 1] == str2[col - 1])
+                        dp[row, col] = dp[row - 1, col - 1];
+                    else
+                    {
+                        int replace = dp[row - 1, col - 1] + replaceCost;
+                        int incert = dp[row, col - 1] + incertCost;
+                        int delete = dp[row - 1, col] + deleteCost;
+
+                        dp[row, col] = Math.Min(Math.Min(replace, incert), delete);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Minimum edit distance: {dp[str1.Length, str2.Length]}");
+        }
+    }
+}
