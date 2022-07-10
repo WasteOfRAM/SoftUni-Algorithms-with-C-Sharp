a13283317a079321ceb7625b2d0c7da218a42b26
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-07-10 14:15:57 +0300

    Problem 2 and 3 solutions

---
+++ Algorithms_Fundamentals_with_C#_June_2022/Introduction_to_Dynamic_Programming/P02.Move_Down_Right/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.Move_Down_Right
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int numRows = int.Parse(Console.ReadLine());
+            int numCols = int.Parse(Console.ReadLine());
+
+            int[,] matrix = MatrixFill(numRows, numCols);
+
+            int[,] dpMatrix = DPMatrix(matrix);
+
+            int row = numRows - 1;
+            int col = numCols - 1;
+
+            var path = new Stack<string>();
+
+            while (row > 0 && col > 0)
+            {
+                path.Push($"[{row}, {col}]");
+
+                int upper = dpMatrix[row - 1, col];
+                int left = dpMatrix[row, col -1];
+
+                if (upper > left)
+                {
+                    row -= 1;
+                }
+                else
+                {
+                    col -= 1;
+                }
+            }
+
+            while (row > 0)
+            {
+                path.Push($"[{row}, {col}]");
+                row -= 1;
+            }
+
+            while (col > 0)
+            {
+                path.Push($"[{row}, {col}]");
+                col -= 1;
+            }
+
+            path.Push($"[{row}, {col}]");
+            Console.WriteLine(string.Join(" ", path));
+        }
+
+        private static int[,] DPMatrix(int[,] matrix)
+        {
+            var dp = new int[matrix.GetLength(0), matrix.GetLength(1)];
+
+            dp[0, 0] = matrix[0, 0];
+
+            for (int col = 1; col < dp.GetLength(1); col++)
+            {
+                dp[0, col] = dp[0, col - 1] + matrix[0, col];
+            }
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                dp[row, 0] = dp[row - 1, 0] + matrix[row, 0];
+            }
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                for (int col = 1; col < dp.GetLength(1); col++)
+                {
+                    int upper = matrix[row, col] + dp[row - 1, col];
+                    int left = matrix[row, col] + dp[row, col - 1];
+
+                    dp[row, col] = Math.Max(upper, left);
+                }
+            }
+
+            return dp;
+        }
+
+        private static int[,] MatrixFill(int numRows, int numCols)
+        {
+            var matrix = new int[numRows, numCols];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = line[j];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ Algorithms_Fundamentals_with_C#_June_2022/Introduction_to_Dynamic_Programming/P03.Longest_Common_Subsequence/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03.Longest_Common_Subsequence
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+
+            var lcs = new int[str1.Length + 1, str2.Length + 1];
+
+            for (int row = 1; row < lcs.GetLength(0); row++)
+            {
+                for (int col = 1; col < lcs.GetLength(1); col++)
+                {
+                    if (str1[row - 1] == str2[col - 1])
+                        lcs[row, col] = lcs[row - 1, col - 1] + 1;
+                    else
+                        lcs[row, col] = Math.Max(lcs[row, col - 1], lcs[row - 1, col]);
+                }
+            }
+
+            Console.WriteLine(lcs[str1.Length, str2.Length]);
+
+            //var sequence = new Stack<char>();
+
+            //var rows = lcs.GetLength(0) - 1;
+            //var cols = lcs.GetLength(1) - 1;
+
+            //while (rows > 0 && cols > 0)
+            //{
+            //    if (str1[rows - 1] == str2[cols - 1] && lcs[rows, cols] == lcs[rows - 1, cols -1] + 1)
+            //    {
+            //        sequence.Push(str1[rows - 1]);
+
+            //        rows -= 1;
+            //        cols -= 1;
+            //    }
+            //    else if (lcs[rows - 1, cols] > lcs[rows, cols - 1])
+            //    {
+            //        rows -= 1;
+            //    }
+            //    else
+            //    {
+            //        cols -= 1;
+            //    }
+            //}
+
+            //Console.WriteLine(string.Join("", sequence));
+        }
+    }
+}
