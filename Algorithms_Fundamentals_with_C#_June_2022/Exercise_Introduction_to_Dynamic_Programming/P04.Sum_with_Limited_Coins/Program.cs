655f23896806edfe5701421f2d29ecd712743921
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-07-10 20:41:04 +0300

    Problems 3 and 4 solutions.

---
+++ Algorithms_Fundamentals_with_C#_June_2022/Exercise_Introduction_to_Dynamic_Programming/P04.Sum_with_Limited_Coins/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P04.Sum_with_Limited_Coins
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int target = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SumsCount(numbers, target));
+        }
+
+        private static int SumsCount(int[] numbers, int target)
+        {
+            int count = 0;
+
+            var sums = new HashSet<int> { 0 };
+
+            foreach (var number in numbers)
+            {
+                var newSums = new HashSet<int>();
+
+                foreach (var sum in sums)
+                {
+                    int newSum = sum + number;
+
+                    if (newSum == target)
+                        count++;
+
+                    newSums.Add(newSum);
+                }
+
+                sums.UnionWith(newSums);
+            }
+
+            return count;
+        }
+    }
+}
