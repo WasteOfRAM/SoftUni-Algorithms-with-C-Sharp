a28cc5b177d72de0cdde0d0b420a3c55a7a3356e
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-07-24 14:54:50 +0300

    Problems 1 and 3 solutions

---
+++ Algorithms_Fundamentals_with_C#_June_2022/Exam/P01.Trains/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.Trains
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double[] arrivalTimes = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            double[] departureTimes = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            int timesCount = arrivalTimes.Length;
+
+            Array.Sort(arrivalTimes);
+            Array.Sort(departureTimes);
+
+            int platforms = 1;
+            int result = 1;
+            int i = 1;
+            int j = 0;
+
+            while (i < timesCount && j < timesCount)
+            {
+                if (arrivalTimes[i] < departureTimes[j])
+                {
+                    platforms++;
+                    i++;
+
+                    if (result < platforms)
+                    {
+                        result = platforms;
+                    }
+                }
+                else
+                {
+                    platforms--;
+                    j++;
+                }
+            }
+            
+
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Algorithms_Fundamentals_with_C#_June_2022/Exam/P03.Guards/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.Guards
+{
+    internal class Program
+    {
+        private static List<int>[] graph;
+        private static bool[] visited;
+
+        static void Main()
+        {
+            int nodesCount = int.Parse(Console.ReadLine());
+            int edgeCount = int.Parse(Console.ReadLine());
+
+            graph = new List<int>[nodesCount + 1];
+            visited = new bool[nodesCount + 1];
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                graph[i] = new List<int>();
+            }
+
+            for (int i = 1; i <= edgeCount; i++)
+            {
+                int[] edge = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int parent = edge[0];
+                int child = edge[1];
+
+                graph[parent].Add(child);
+            }
+
+            int startNode = int.Parse(Console.ReadLine());
+
+            DFS(startNode);
+
+            var unreachable = new HashSet<int>();
+
+            for (int i = 1; i < visited.Length; i++)
+            {
+                if (!visited[i])
+                    unreachable.Add(i);
+            }
+
+            Console.WriteLine(string.Join(" ", unreachable));
+        }
+
+        private static void DFS(int startNode)
+        {
+            if (graph[startNode].Count == 0)
+                return;
+
+            visited[startNode] = true;
+
+            foreach (var child in graph[startNode])
+            {
+                if (!visited[child])
+                {
+                    visited[child] = true;
+                    DFS(child);
+                }
+            }
+        }
+    }
+}
