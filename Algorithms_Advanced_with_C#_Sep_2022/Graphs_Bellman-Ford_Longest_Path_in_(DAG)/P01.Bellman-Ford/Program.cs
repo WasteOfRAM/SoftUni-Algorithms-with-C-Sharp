d82a1c35942dda74c30ab4d32ce6e781d6289d57
Ivan Kukutsov <ivankukucov@gmail.com>
2022-09-05 23:17:41 +0300

    Bellman-Ford problem

---
+++ Algorithms_Advanced_with_C#_Sep_2022/Graphs_Bellman-Ford_Longest_Path_in_(DAG)/P01.Bellman-Ford/Program.cs
@@ -0,0 +1,112 @@
+﻿namespace P01.Bellman_Ford
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    class Edge
+    {
+        public int From { get; set; }
+        public int To { get; set; }
+        public int Weight { get; set; }
+    }
+
+    internal class Program
+    {
+        private static List<Edge> graph;
+        private static double[] distances;
+        private static int[] prev;
+
+        static void Main()
+        {
+            graph = new List<Edge>();
+
+            var nodesCount = int.Parse(Console.ReadLine());
+            var edgesCount = int.Parse(Console.ReadLine());
+
+            distances = new double[nodesCount + 1];
+            Array.Fill(distances, double.PositiveInfinity);
+
+            prev = new int[nodesCount +1];
+            Array.Fill(prev, -1);
+
+            for (int i = 0; i < edgesCount; i++)
+            {
+                var edgeData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                graph.Add(new Edge{From = edgeData[0], To = edgeData[1], Weight = edgeData[2] });
+            }
+
+            var startNode = int.Parse(Console.ReadLine());
+            var destinationNode = int.Parse(Console.ReadLine());
+
+            distances[startNode] = 0;
+
+            try
+            {
+                BellmanFord(nodesCount);
+
+                var path = PathExtraction(destinationNode);
+
+                Console.WriteLine(string.Join(" ", path));
+                Console.WriteLine(distances[destinationNode]);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            
+        }
+
+        private static Stack<int> PathExtraction(int destinationNode)
+        {
+            var path = new Stack<int>();
+            var currentNode = destinationNode;
+
+            while (currentNode != -1)
+            {
+                path.Push(currentNode);
+                currentNode = prev[currentNode];
+            }
+
+            return path;
+        }
+
+        private static void BellmanFord(int nodesCount)
+        {
+            for (int i = 0; i < nodesCount - 1; i++)
+            {
+                var updated = false;
+
+                foreach (var edge in graph)
+                {
+                    if (double.IsPositiveInfinity(distances[edge.From]))
+                    {
+                        continue;
+                    }
+
+                    var newDistance = distances[edge.From] + edge.Weight;
+                    if (newDistance < distances[edge.To])
+                    {
+                        distances[edge.To] = newDistance;
+                        prev[edge.To] = edge.From;
+                        updated = true;
+                    }
+                }
+
+                if(!updated)
+                    break;
+            }
+
+            foreach (var edge in graph)
+            {
+                var newDistance = distances[edge.From] + edge.Weight;
+                if (newDistance < distances[edge.To])
+                {
+                    throw new ArgumentException("Negative Cycle Detected");
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms_Advanced_with_C#_Sep_2022/Graphs_Bellman-Ford_Longest_Path_in_(DAG)/P02.Longest_Path/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.ComponentModel.Design;
+
+namespace P02.Longest_Path
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Edge
+    {
+        public int From { get; set; }
+        public int To { get; set; }
+        public int Weight { get; set; }
+    }
+
+    internal class Program
+    {
+        private static Dictionary<int, List<Edge>> edgesByNode;
+        private static double[] distances;
+
+        static void Main()
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var edges = int.Parse(Console.ReadLine());
+
+            edgesByNode = new Dictionary<int, List<Edge>>();
+
+            distances = new double[nodes + 1];
+            Array.Fill(distances, double.NegativeInfinity);
+
+            for (int i = 0; i < edges; i++)
+            {
+                var edgeData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                var from = edgeData[0];
+                var to = edgeData[1];
+                var weight = edgeData[2];
+
+                if(!edgesByNode.ContainsKey(from))
+                    edgesByNode[from] = new List<Edge>();
+
+                if (!edgesByNode.ContainsKey(to))
+                    edgesByNode[to] = new List<Edge>();
+
+                edgesByNode[from].Add(new Edge{From = from, To = to, Weight = weight});
+            }
+
+            var start = int.Parse(Console.ReadLine());
+            var destination = int.Parse(Console.ReadLine());
+
+            distances[start] = 0;
+
+            var sorted = TopologicalSort();
+
+            LongestPath(sorted);
+
+            Console.WriteLine(distances[destination]);
+        }
+
+        private static void LongestPath(Stack<int> sorted)
+        {
+            while (sorted.Count > 0)
+            {
+                var nodes = sorted.Pop();
+
+                foreach (var edge in edgesByNode[nodes])
+                {
+                    var newDistance = distances[edge.From] + edge.Weight;
+
+                    if (newDistance > distances[edge.To])
+                        distances[edge.To] = newDistance;
+                }
+            }
+        }
+
+        private static Stack<int> TopologicalSort()
+        {
+            var result = new Stack<int>();
+            var visited = new HashSet<int>();
+
+            foreach (var node in edgesByNode.Keys)
+            {
+                DFS(node, visited, result);
+            }
+
+            return result;
+        }
+
+        private static void DFS(int node, HashSet<int> visited, Stack<int> result)
+        {
+            if(visited.Contains(node))
+                return;
+            
+            visited.Add(node);
+
+            foreach (var edge in edgesByNode[node])
+            {
+                DFS(edge.To, visited, result);
+            }
+
+            result.Push(node);
+        }
+    }
+}
