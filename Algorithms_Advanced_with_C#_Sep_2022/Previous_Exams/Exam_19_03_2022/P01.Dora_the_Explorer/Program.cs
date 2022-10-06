af4356d073c791ed2447eede61f44c5e692f5492
Ivan Kukutsov <ivankukucov@gmail.com>
2022-10-07 01:54:09 +0300

    Problems 1 and 2

---
+++ Algorithms_Advanced_with_C#_Sep_2022/Previous_Exams/Exam_19_03_2022/P01.Dora_the_Explorer/Program.cs
@@ -0,0 +1,123 @@
+﻿namespace P01.Dora_the_Explorer
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using Wintellect.PowerCollections;
+
+    class Edge
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Weight { get; set; }
+    }
+
+    internal class Program
+    {
+        private static Dictionary<int, List<Edge>> edgesByNode;
+        private static int[] prev;
+        private static double[] time;
+
+        static void Main()
+        {
+            var edgeCount = int.Parse(Console.ReadLine());
+
+            edgesByNode = new Dictionary<int, List<Edge>>();
+
+
+            for (int i = 0; i < edgeCount; i++)
+            {
+                var edgeArgs = Console.ReadLine()
+                    .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var nodeOne = edgeArgs[0];
+                var nodeTwo = edgeArgs[1];
+                var weight = edgeArgs[2];
+
+                var edge = new Edge { First = nodeOne, Second = nodeTwo, Weight = weight };
+
+                if (!edgesByNode.ContainsKey(nodeOne))
+                    edgesByNode[nodeOne] = new List<Edge>();
+
+                if (!edgesByNode.ContainsKey(nodeTwo))
+                    edgesByNode[nodeTwo] = new List<Edge>();
+
+                edgesByNode[nodeOne].Add(edge);
+                edgesByNode[nodeTwo].Add(edge);
+            }
+
+            var timeSpent = int.Parse(Console.ReadLine());
+
+            var startNode = int.Parse(Console.ReadLine());
+            var endNode = int.Parse(Console.ReadLine());
+
+            var nodesCount = edgesByNode.Keys.Max() + 1;
+
+            time = new double[nodesCount];
+            prev = new int[nodesCount];
+
+            for (int i = 0; i < prev.Length; i++)
+            {
+                prev[i] = -1;
+            }
+
+            for (int node = 0; node < time.Length; node++)
+            {
+                time[node] = double.PositiveInfinity;
+            }
+
+            time[startNode] = 0;
+
+            var priorityQueue = new OrderedBag<int>(Comparer<int>.Create((firstNode, secondNode) => (int)(time[firstNode] - time[secondNode]))) { startNode };
+
+            DijkstraBfs(priorityQueue, endNode, timeSpent);
+
+            var path = new Stack<int>();
+            var currentNode = endNode;
+
+            while (currentNode != -1)
+            {
+                path.Push(currentNode);
+                currentNode = prev[currentNode];
+            }
+
+            Console.WriteLine($"Total time: {time[endNode] - timeSpent}");
+            Console.WriteLine(string.Join(Environment.NewLine, path));
+
+        }
+
+        private static void DijkstraBfs(OrderedBag<int> priorityQueue, int endNode, int timeSpent)
+        {
+            while (priorityQueue.Count != 0)
+            {
+                var minNode = priorityQueue.RemoveFirst();
+
+                if(minNode == endNode || double.IsPositiveInfinity(time[minNode]))
+                {
+                    break;
+                }
+
+                foreach (var edge in edgesByNode[minNode])
+                {
+                    var otherNode = edge.First == minNode ? edge.Second : edge.First;
+
+                    if (double.IsPositiveInfinity(time[otherNode]))
+                        priorityQueue.Add(otherNode);
+
+                    var newTime = time[minNode] + edge.Weight + timeSpent;
+
+                    if (!(newTime < time[otherNode])) continue;
+
+                    prev[otherNode] = minNode;
+                    time[otherNode] = newTime;
+
+                    priorityQueue = new OrderedBag<int>(priorityQueue,
+                        Comparer<int>.Create((firstNode, secondNode) =>
+                            (int)(time[firstNode] - time[secondNode])));
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms_Advanced_with_C#_Sep_2022/Previous_Exams/Exam_19_03_2022/P02.Bitcoin_Mining/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace P02.Bitcoin_Mining
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    class Transaction
+    {
+        public string Hash { get; set; }
+        public int Size { get; set; }
+        public int Fees { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+
+        public override string ToString()
+        {
+            return this.Hash;
+        }
+    }
+
+    internal class Program
+    {
+
+        static void Main()
+        {
+            var transactions = new List<Transaction>();
+            var usedTransactions = new List<Transaction>();
+
+            var blockCapacity = 1000000;
+
+        var pendingTransactions = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < pendingTransactions; i++)
+            {
+                var transactionInfo = Console.ReadLine().Split(" ");
+                var hash = transactionInfo[0];
+                var size = int.Parse(transactionInfo[1]);
+                var fees = int.Parse(transactionInfo[2]);
+                var from = transactionInfo[3];
+                var to = transactionInfo[4];
+
+                transactions.Add(new Transaction { Hash = hash, Size = size, Fees = fees, From = from, To = to });
+            }
+
+            var dp = new int[transactions.Count + 1, blockCapacity + 1];
+            var selectedTransactions = new bool[transactions.Count + 1, blockCapacity + 1];
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                var currentTransaction = transactions[row - 1];
+                for (int capacity = 1; capacity < dp.GetLength(1); capacity++)
+                {
+                    var excluding = dp[row - 1, capacity];
+                    if (currentTransaction.Size > capacity)
+                    {
+                        dp[row, capacity] = excluding;
+                        continue;
+                    }
+
+                    var including = currentTransaction.Fees + dp[row - 1, capacity - currentTransaction.Size];
+
+                    if (including > excluding)
+                    {
+                        dp[row, capacity] = including;
+                        selectedTransactions[row, capacity] = true;
+                    }
+                    else
+                    {
+                        dp[row, capacity] = excluding;
+                    }
+                }
+            }
+
+            for (int row = dp.GetLength(0) - 1; row >= 0; row--)
+            {
+                if (selectedTransactions[row, blockCapacity])
+                {
+                    var transaction = transactions[row - 1];
+                    usedTransactions.Add(transaction);
+                    blockCapacity -= transaction.Size;
+                }
+            }
+
+            var totalSize = usedTransactions.Sum(t => t.Size);
+            var totalFees = usedTransactions.Sum(t => t.Fees);
+
+            Console.WriteLine($"Total Size: {totalSize}");
+            Console.WriteLine($"Total Fees: {totalFees}");
+
+            Console.WriteLine(string.Join(Environment.NewLine, usedTransactions));
+        }
+    }
+}
